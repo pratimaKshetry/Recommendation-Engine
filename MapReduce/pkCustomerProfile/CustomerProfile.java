3b95e51ac807c66e5526ebf22f596d6775bf796b
Pratima Kshetry <pratima.kshetry@hotmail.com>
2015-12-18 22:53:28 -0500

    Remove old CustomerProfile

--- MapReduce/pkCustomerProfile/CustomerProfile.java
+++
@@ -1,8 +0,0 @@
-package pkCustomerProfile;
-
-public class CustomerProfile {
-	public String id;
-	//productDataString contains string info of product format
-	//productID|rating|productTitle|similarproductID
-	public String productDataString;
-}
--- MapReduce/pkCustomerProfile/ProductProfile.java
+++
@@ -1,8 +0,0 @@
-package pkCustomerProfile;
-
-public class ProductProfile {
-	public String id;
-	public String title;
-	public String avgRating;	
-	public String similarProductIDs;
-}
